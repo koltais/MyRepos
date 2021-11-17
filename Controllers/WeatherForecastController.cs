2b25beb8015a0db1072d0d06fa7d4c235f01a618
koltais <koltai.sandor@tfszzrt.hu>
2021-11-17 15:12:02 +0100

    json try

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace WebApplication2.Controllers
 {
@@ -71,5 +71,33 @@ namespace WebApplication2.Controllers
 
             return Ok();
         }
+
+        [HttpGet]
+        [Route("/getDT")]
+        public IEnumerable<MyModel> GetDtJson()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            var jsonString = System.IO.File.ReadAllText(Environment.CurrentDirectory.ToString() + "\\my-model.json");
+            var jsonModel = JsonSerializer.Deserialize<MyModel>(jsonString, options);
+
+            jsonModel.MyString = "New test";
+            jsonModel.MyDateTime1 = DateTime.UtcNow;
+            jsonModel.MyInt = 9999999;
+            jsonModel.MyAnotherModel.MyStringList = new List<string> { "coconut", "grain" };
+
+            var modelJson = JsonSerializer.Serialize(jsonModel, options);
+            System.IO.File.WriteAllText(Environment.CurrentDirectory.ToString() + "\\my-model.json", modelJson);
+
+            return new List<MyModel>
+            {
+                jsonModel,jsonModel.MyAnotherModel
+            };
+        }
+
     }
 }
