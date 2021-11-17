6019349c69822c676bb8c174c027cc8b32cc9e77
koltais <koltai.sandor@tfszzrt.hu>
2021-11-17 08:56:27 +0100

    headers

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -55,9 +55,17 @@ namespace WebApplication2.Controllers
         public IActionResult SetParam(string sParam)
         {
             string s = Request.QueryString.ToString();
+            var hd = Enumerable.ToDictionary(Request.Headers, x => x.Key, x => x.Value);
+
             _logger.LogInformation("SetParam worker running at: {time}", DateTimeOffset.UtcNow);
             _logger.LogInformation("SetParam QuersString: {querstring}", s);
             _logger.LogInformation("SetParam POSTed param: {param}", sParam);
+
+            foreach(var i in hd)
+            {
+                _logger.LogInformation("Header ({key}): {head}", i.Key, i.Value);
+            }
+
             return Ok();
         }
     }
